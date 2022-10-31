5aa8af11059fc65199988118444dd8a8fb844b0e
tolgaozkann <ozkanozkan3233@gmail.com>
2022-10-31 20:19:22 +0300

    json mapper libaries added

--- Java-Sorting-Algorithms/src/App.java
+++ Java-Sorting-Algorithms/src/App.java
@@ -1,5 +1,12 @@
+import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
+import com.fasterxml.jackson.annotation.JsonProperty; // version 2.11.1
+
+
+
 public class App {
     public static void main(String[] args) throws Exception {
+
+        ObjectMapper om = new ObjectMapper();
         System.out.println("Hello, World!");
     }
 }
